5a63296925c8b7bfb9816eaa16f54198948cf59d
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-10-05 22:48:37 +0800

    Change-Project-to-flex-laytou

--- project/static/js/login/script.js
+++ project/static/js/login/script.js
@@ -1,11 +1,11 @@
 $('#sign_up').click(function() {
-  $('.move_box').css('transform', 'translateX(80%)');
-  $('.sign_in').addClass('hidden');
-  $('.sign_up').removeClass('hidden');
+  $('.login-move_box').css('transform', 'translateX(80%)');
+  $('.sign_in').addClass('login-hidden');
+  $('.sign_up').removeClass('login-hidden');
 });
 
 $('#sign_in').click(function() {
-  $('.move_box').css('transform', 'translateX(0%)');
-  $('.sign_up').addClass('hidden');
-  $('.sign_in').removeClass('hidden');
+  $('.login-move_box').css('transform', 'translateX(0%)');
+  $('.sign_up').addClass('login-hidden');
+  $('.sign_in').removeClass('login-hidden');
 });
