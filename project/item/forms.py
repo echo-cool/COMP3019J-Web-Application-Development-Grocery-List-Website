22db7fe7328ec055f6c2f760c8cb1d5a08fa7a72
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-10-08 14:29:47 +0800

    Add-item-form

--- project/item/forms.py
+++ project/item/forms.py
@@ -1,5 +1,6 @@
 from flask_admin.form import FileUploadField
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileRequired, FileAllowed
 from wtforms import StringField, IntegerField, FloatField, SubmitField, FileField
 from wtforms.fields.html5 import URLField
 from wtforms.validators import DataRequired
@@ -12,5 +13,8 @@ class AddNewItem(FlaskForm):
     description = StringField("Description", validators=[], default="")
     inventory = IntegerField("Inventory", validators=[], default=0)
     # main_image_url = URLField("main_image_url", validators=[], default="")
-    main_image_file = FileField("Upload Image")
+    main_image_file = FileField("Upload Image", validators=[
+        FileRequired(),
+        FileAllowed(['png', 'jpg', 'jpeg'], 'Only allow png, jpg')
+    ])
     add_button = SubmitField("Add this item")
