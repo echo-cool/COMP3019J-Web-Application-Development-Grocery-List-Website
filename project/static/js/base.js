d5570a1931cfc08310aa5bad691ac750f96bdd6f
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-10-20 16:31:03 +0800

    Fire-work

--- project/static/js/base.js
+++ project/static/js/base.js
@@ -3,7 +3,7 @@ $(window).scroll(function () {
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     var window_height = $(window).height();
     var index_images = $(".scroll-fade-in");
-    for (let i = 0; i < index_images.size(); i++) {
+    for (let i = 0; i < index_images.length; i++) {
         if (index_images[i].offsetTop < scrollTop + window_height) {
             if (index_images[i] != null) {
                 index_images[i].style.animation = "blurFadeIn 0.5s ease-in 0s backwards";
@@ -23,66 +23,66 @@ $(window).scroll(function () {
 //Save the original value of the block
 var counters = $(".anim-counter");
 var orgi_number = new Array();
-for (let i = 0; i < counters.size(); i++) {
+for (let i = 0; i < counters.length; i++) {
     var number = Number(counters[i].innerText);
     orgi_number[i] = number;
-    counters[i].innerText=0;
+    counters[i].innerText = 0;
 }
 
 //start timer to add the value to the original value
 var timer = setInterval(() => {
         var counters = $(".anim-counter");
-        for (let i = 0; i < counters.size(); i++) {
+        for (let i = 0; i < counters.length; i++) {
             var number = Number(counters[i].innerText);
-            if(number < orgi_number[i]){
-                counters[i].innerText = number + parseInt(orgi_number[i]/100);
+            if (number < orgi_number[i]) {
+                counters[i].innerText = number + parseInt(orgi_number[i] / 100);
             }
-            if(number > orgi_number[i]){
-                 counters[i].innerText = orgi_number[i];
+            if (number > orgi_number[i]) {
+                counters[i].innerText = orgi_number[i];
             }
         }
     },
     10
 );
 
-
-function co() {
-  var colorElements = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
-  var colorArray = colorElements.split(",");
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += colorArray[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function gen_color() {
+    var hex_numbers = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
+    var colorArray = hex_numbers.split(",");
+    var res = "#";
+    for (var i = 0; i < 6; i++) {
+        res += colorArray[Math.floor(Math.random() * 16)];
+    }
+    return res;
 };
 var a_idx = 0;
 jQuery(document).ready(function ($) {
-  $("body").click(function (e) {
-
-    var config = new Array(["Hi", "Welcome"]);
-    var a = config.text.split(",");
-    var $i = $("<span/>").text(a[a_idx]);
-    a_idx = (a_idx + 1) % a.length;
-    var x = e.pageX,
-      y = e.pageY;
-    $i.css({
-      "z-index": 150,
-      "top": y - 20,
-      "left": x - 40,
-      "position": "absolute",
-      "font-weight": "bold",
-      "color": co(),
-      "cursor": "default",
-      "font-size": config.fontSize || "inherit"
+    $("body").click(function (e) {
+        var text_data = "Hello,Welcome,Shopping";
+        var a = text_data.split(",");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+            y = e.pageY;
+        $i.css({
+            "z-index": 150,
+            "top": y - 20,
+            "left": x - 40,
+            "position": "absolute",
+            "font-weight": "bold",
+            "cursor": "default",
+            "color": gen_color(),
+            "font-size": "20px",
+            "animation": "wiggle 1.5s ease-in 0s backwards"
+        });
+        $("body").append($i);
+        $i.animate({
+                "top": y - 180,
+                "opacity": 0
+            },
+            1500,
+            function () {
+                $i.remove();
+            });
     });
-    $("body").append($i);
-    $i.animate({
-      "top": y - 180,
-      "opacity": 0
-    },
-      1500,
-      function () {
-        $i.remove();
-      });
-  });
 });
+
---
+++ project/static/js/fireworks.js
@@ -0,0 +1,72 @@
+var canvasEl = document.querySelector('.fireworks')
+if (canvasEl) {
+    var ctx = canvasEl.getContext('2d')
+    var pointerX = 0
+    var pointerY = 0
+    var tap = 'mousedown'
+    var colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C', '#FBF38C']
+
+
+    canvasEl.width = window.innerWidth
+    canvasEl.height = window.innerHeight
+    canvasEl.style.width = window.innerWidth + 'px'
+    canvasEl.style.height = window.innerHeight + 'px'
+    canvasEl.getContext('2d').scale(1, 1)
+
+    document.addEventListener(tap, function (e) {
+        if (e.target.id !== 'sidebar' && e.target.id !== 'toggle-sidebar' && e.target.nodeName !== 'A' && e.target.nodeName !== 'IMG') {
+            updateCoords(e)
+            animateParticules(pointerX, pointerY)
+        }
+    }, false)
+}
+
+function updateCoords(e) {
+    pointerX = (e.clientX || e.touches[0].clientX) - canvasEl.getBoundingClientRect().left
+    pointerY = e.clientY || e.touches[0].clientY - canvasEl.getBoundingClientRect().top
+}
+
+function animateParticules(x, y) {
+
+    canvasEl.style = "animation: Fadeout 0.5s ease-in 0s backwards;"
+    var run_color = new Array();
+    for (let count = 0; count < 40; count++) {
+        run_color[count] = colors[Number.parseInt(Math.random() * 5)];
+    }
+    for (let i = 0; i < 100; i++) {
+
+        setTimeout(function () {
+            clearCanvas();
+            ctx.closePath()
+            var radious1 = i * 2;
+            var radious2 = i * 3;
+
+            count = 0;
+            for (let r = 0.0; r < 2 * Math.PI; r += Math.PI / 10) {
+                var delta_x = Math.sin(r);
+                var delta_y = Math.cos(r);
+
+                // ctx.arc(x + radious1 * delta_x, y + radious1 * delta_y, 10 - i / 10, 0, Math.PI * 2, true);
+                // ctx.closePath();
+                // ctx.fillStyle = "green";
+                // ctx.fill();
+                ctx.fillStyle = run_color[count];
+                ctx.fillRect(x + radious1 * delta_x, y + radious1 * delta_y, 10 - i / 10, 10 - i / 10);
+                ctx.fillRect(x + radious2 * delta_x, y + radious2 * delta_y, 20 - i / 5, 20 - i / 5);
+                count += 1;
+            }
+
+        }, i * 5);
+    }
+    setTimeout(function () {
+        clearCanvas();
+        canvasEl.style = "animation: none;"
+    }, 99 * 5)
+
+
+}
+
+function clearCanvas() {
+    var cxt = canvasEl.getContext("2d");
+    cxt.clearRect(0, 0, canvasEl.width, canvasEl.height);
+}
