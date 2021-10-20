d108b49e773376302491ebb94ff30a81366de21d
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-10-20 15:01:57 +0800

    base.css animation

--- project/static/js/base.js
+++ project/static/js/base.js
@@ -46,3 +46,43 @@ var timer = setInterval(() => {
 );
 
 
+function co() {
+  var colorElements = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
+  var colorArray = colorElements.split(",");
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += colorArray[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+var a_idx = 0;
+jQuery(document).ready(function ($) {
+  $("body").click(function (e) {
+
+    var config = new Array(["Hi", "Welcome"]);
+    var a = config.text.split(",");
+    var $i = $("<span/>").text(a[a_idx]);
+    a_idx = (a_idx + 1) % a.length;
+    var x = e.pageX,
+      y = e.pageY;
+    $i.css({
+      "z-index": 150,
+      "top": y - 20,
+      "left": x - 40,
+      "position": "absolute",
+      "font-weight": "bold",
+      "color": co(),
+      "cursor": "default",
+      "font-size": config.fontSize || "inherit"
+    });
+    $("body").append($i);
+    $i.animate({
+      "top": y - 180,
+      "opacity": 0
+    },
+      1500,
+      function () {
+        $i.remove();
+      });
+  });
+});
