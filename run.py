a2dc261e21eb406468edf34fe73d885f3a36c69c
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-10-22 17:56:31 +0800

    add-port-def

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+from os import environ
+
 from project import app
 
 # Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=environ.get("PORT", 5000))
