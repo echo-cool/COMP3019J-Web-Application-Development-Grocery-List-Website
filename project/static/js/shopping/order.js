da3cd5eb3536ae078e176f5ea095af1354fdf8a7
Yang Liuxin <liuxin.yang@ucdconnect.ie>
2021-12-07 19:34:11 +0800

    add js to check order confirmation

---
+++ project/static/js/shopping/order.js
@@ -0,0 +1,7 @@
+function alertNotConfirmedBySeller(){
+    alert("Sorry you cannot confirm this order because it has not been confirmed by the seller!")
+}
+
+function alertConfirmedBySeller(){
+    alert("That is fine!")
+}
