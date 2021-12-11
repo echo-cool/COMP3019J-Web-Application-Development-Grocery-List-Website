1112ac1b2492f291b014fa4898817ba32956b410
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-12-11 18:23:08 +0800

    login-validate

--- project/user/forms.py
+++ project/user/forms.py
@@ -1,15 +1,15 @@
-
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
 from wtforms import StringField, IntegerField, FloatField, SubmitField, FileField
 from wtforms.fields.html5 import URLField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
+
 
 # This form is used when user want to update his personal information
 class UpdateUser(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), ])
-    email = StringField("Email", validators=[DataRequired(), ])
-    password = StringField("Username", validators=[])
+    username = StringField("Username", validators=[DataRequired(), Length(min=5, max=25)])
+    email = StringField("Email", validators=[DataRequired(), Length(min=5, max=25)])
+    password = StringField("Password", validators=[Length(min=5, max=25)])
     # only allowing user to upload images
     image_file = FileField("Upload Image", validators=[
         FileAllowed(['png', 'jpg', 'jpeg'], 'Only allow png, jpg')
