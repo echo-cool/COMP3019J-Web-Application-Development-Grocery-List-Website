4743b7335b8f2e7a79acfece09118a4606d74bee
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-12-11 16:32:13 +0800

    ajax-add

--- project/static/js/shopping/product_detail.js
+++ project/static/js/shopping/product_detail.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    $(".shopping-cart")
+
+    $(".shopping-cart").on("click", function (e) {
+
+        let itemID = $("#item-id-hidden").val();
+        let quantity = $("#quantity-hidden").val();
+
+        $("#shopping-cart-text-nav")
+
+        $.ajax({
+            url: "/cart/add",
+            type: "POST",
+            data: {
+                itemID: itemID,
+                itemCount: quantity,
+                csrf_token: $("meta[name='csrf-token']").attr("content")
+            },
+            success: function (data) {
+                alert("Item added to cart!");
+            }
+        });
+    });
 });
