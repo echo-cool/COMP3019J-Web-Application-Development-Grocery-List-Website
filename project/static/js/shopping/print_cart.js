a5609acf18e97d3c30e99570cbc01db596d6c7d2
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-12-12 14:14:09 +0800

    fix-cart

--- project/static/js/shopping/print_cart.js
+++ project/static/js/shopping/print_cart.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
     $('#print_cart').click(function () {
+        $('#print_cart').hide();
         window.print();
+        $('#print_cart').show();
     });
     setTimeout(function () {
+        $('#print_cart').hide();
         window.print();
+        $('#print_cart').show();
     }, 300);
 });
