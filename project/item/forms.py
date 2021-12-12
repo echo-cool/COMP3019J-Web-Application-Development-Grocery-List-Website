6f4a86eddf266ae2e290939d12128ab3f43c538e
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-12-12 21:35:27 +0800

    add-new-validators

--- project/item/forms.py
+++ project/item/forms.py
@@ -2,16 +2,16 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
 from wtforms import StringField, IntegerField, FloatField, SubmitField, FileField
 from wtforms.fields.html5 import URLField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 
 
 # This form is used when a user add a new item to the site
 class AddNewItem(FlaskForm):
     # userid = StringField("User ID", validators=[DataRequired(), ])
     item_name = StringField("Item Name", validators=[DataRequired(), Length(min=5, max=25)], default="")
-    item_price = FloatField("Price", validators=[DataRequired()], default=0.0)
+    item_price = FloatField("Price", validators=[DataRequired(), NumberRange(min=0, max=10000000)], default=0.0)
     description = StringField("Description", validators=[DataRequired(), Length(min=5)], default="")
-    inventory = IntegerField("Inventory", validators=[DataRequired()], default=0)
+    inventory = IntegerField("Inventory", validators=[DataRequired(), NumberRange(min=0, max=10000000)], default=0)
     # main_image_url = URLField("main_image_url", validators=[], default="")]
     # Only allow images in the upload field
     main_image_file = FileField("Upload Image", validators=[
@@ -24,9 +24,9 @@ class AddNewItem(FlaskForm):
 # This form is used when a user wants to update the information in the site
 class UpdateItem(FlaskForm):
     item_name = StringField("Item Name", validators=[DataRequired(), Length(min=5, max=25)], default="")
-    item_price = FloatField("Price", validators=[DataRequired()], default=0.0)
+    item_price = FloatField("Price", validators=[DataRequired(), NumberRange(min=0, max=10000000)], default=0.0)
     description = StringField("Description", validators=[DataRequired(), Length(min=5)], default="")
-    inventory = IntegerField("Inventory", validators=[DataRequired()], default=0)
+    inventory = IntegerField("Inventory", validators=[DataRequired(), NumberRange(min=0, max=10000000)], default=0)
     # main_image_url = URLField("main_image_url", validators=[], default="")
     # Only allow images in the upload field
     main_image_file = FileField("Upload Image", validators=[
--- project/user/forms.py
+++ project/user/forms.py
@@ -2,14 +2,14 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
 from wtforms import StringField, IntegerField, FloatField, SubmitField, FileField
 from wtforms.fields.html5 import URLField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 
 # This form is used when user want to update his personal information
 class UpdateUser(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=5, max=25)])
     email = StringField("Email", validators=[DataRequired(), Length(min=5, max=25)])
-    password = StringField("Password", validators=[])
+    password = StringField("Password", validators=[Optional(), Length(min=5, max=25)])
     # only allowing user to upload images
     image_file = FileField("Upload Image", validators=[
         FileAllowed(['png', 'jpg', 'jpeg'], 'Only allow png, jpg')
